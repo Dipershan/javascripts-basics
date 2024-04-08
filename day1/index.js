dd0b26447ecb49c94a78082d534e24eb683f78d7
Dipershan <dipsestha321@gmail.com>
2024-04-08 16:36:27 +0545

    day 1

---
+++ day1/index.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+/*
+
+variable start
+keywords :  var , let , const
+var(used for globa; variable definition)
+let (reuse variable)
+const (value constant)
+
+*/
+
+const person = "Dipershan Shrestha";
+const number = 4;
+
+//print the number
+console.log(number);
+console.log({ person });
+
+alert("Hello world");
+const question1 = prompt("What is your name");
+
+const question2 = confirm("Are you 18");
+
+console.log({ question1, question2 });
+
+alert(question1 + "is ur name " + question2 + " above 18");
+
+//write a multiplication calculator for 3 digits
