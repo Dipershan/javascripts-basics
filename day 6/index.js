dc7e7264359efc656d6817453af18427357d0099
Dipershan <dipsestha321@gmail.com>
2024-04-20 17:27:47 +0545

    day 6

--- day 6/index.js
+++ day 6/index.js
@@ -94,9 +94,120 @@ const people  = [
     {name:"ram" ,  age:  15},
 ]
 
+const  resul = people.sort((a, b) => a.age - b.age);
+
+console.log(resul); 
 //sort in Ascending orfer by age 
+// const array1 = [1, 30, 4, 21, 100000];
+// array1.sort();
+// console.log(array1);
+
+
+
 
 
 // let food = ['Noodle', 'Pasta', 'Ice-cream'];
 // let food = ['Fries', 'Ice-cream', 'Pizza'];
 // Compare the 2 arrays and find common food if any.
+
+
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+// Get the total mass of all characters
+// Get the total height of all characters
+// Get the total number of characters in all the character names
+//{blue :}
+// Get the total number of characters by eye color (hint. a map of eye color to count)
+
+const callbackFn = (intialValue , currentValue)  =>
+ intialValue + Number(currentValue.mass);
+const getTotalMass = characters.reduce(callbackFn , 0);
+
+console.log(getTotalMass);
+
+const getMass = (arr) => {
+    return arr.reduce(
+        (intialValue , currentValue) => intialValue + Number(currentValue.mass),
+        0
+    );
+};
+
+
+// const eyes  =  characters.reduce((initial , current) =>){
+//     if(initial[curren.eye_color])
+//     {
+//         initial[current.eye_color]++;
+//     }
+//     else
+//     {
+//         initial[current.eye_color] = 1;
+//     }
+//     return newObj;
+// }
+
+
+
+// const resultA =  getMass(characters);
+// console.log({resultA});
+
+// const prices = [5,10,4,6,7,8,19,34];
+
+// const total =  price.reduce(sum);
+
+// function sum(accumulator ,  element){
+//     return accumulator + element ; 
+// }
+
+// const grades = [5,10,4,6,7,8,19,34];
+
+// const maximum =  grades.reduce();
+// console.log(maximum);
+// const getMax = (accumulator  ,  element) =>{
+//     return Math.max(accumulator , element);
+// }
+
+
+
+//Get array of objects with just name and height properties
+
+// const objArr =  characters.map((character)=> {
+//     return { name : character.name ,  height: character.height};
+// });
+
+// console.log(objArr);
+
+
+
+//IMMUTABLE JS
+
+
+//map ,  reduce  ,  sort ,  some  ,  every , filter , find
